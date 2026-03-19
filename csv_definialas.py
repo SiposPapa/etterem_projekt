ba2cd1f4718d9602237e80d06812bd69bba73f2a
SiposPapa <roland.sipos.100@gmail.com>
2026-03-19 11:08:46 +0100

    Update csv_definialas.py
    Egyelőre késznek tűnik

--- csv_definialas.py
+++ csv_definialas.py
@@ -10,3 +10,24 @@ class Asztal:
         self.rendelesek = []    #Követi, hogy ki mit rendelt az adott asztalnál
         self.nyitva = False
 
+    def hozzaad(self, etel): #Egy Etel objektumot ad, és az asztala True lesz
+            self.rendelesek.append(etel)
+            self.nyitva = True
+ 
+    def osszeg_szamitas(self):
+        # Összegzés tétele
+        osszesen = 0
+        for etel in self.rendelesek:
+            osszesen += etel.ar
+        return osszesen
+    def lezaras(self):
+        #Összegszámítás, függvény meghívás
+        osszeg = self.osszeg_szamitas()
+        # Listakészítés            
+        nevek_listaja = []
+        for etel in self.rendelesek:
+            nevek_listaja.append(etel.nev)
+        #Adatok alaphelyzetbe állítása, asztal nyitása
+        self.rendelesek = []
+        self.nyitva = False
+        return osszeg, nevek_listaja
